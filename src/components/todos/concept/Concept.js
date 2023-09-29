6f3bd2c128bcc3c1f60d73bd78dffba95e5c0607
Lumala Brian <lumalabravo@gmail.com>
2023-09-29 10:41:42 +0300

    todo completed first part

---
+++ src/components/todos/concept/Concept.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Concept(props) {
+  return (
+    <li className="concept">
+      <img src={props.image} alt={props.title} />
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </li>
+  );
+}
--- src/components/todos/todos.js
+++ src/components/todos/todos.js
@@ -1,30 +1,28 @@
-import keyConceptsImage from "./../../assets/images/key-concepts.png";
 import { concepts } from "../../data/data";
+import Concept from "./concept/Concept";
+import Header from "./header/Header";
 
 function Todos() {
   return (
     <div>
-      <header>
-        <img src={keyConceptsImage} alt="Medal badge with a star" />
-        <h1>Key React Concepts</h1>
-        <p>Selected key React concepts you should know about</p>
-      </header>
+      {/* importing header */}
+      <Header />
       <ul id="concepts">
-        <li className="concept">
-          <img src={concepts[0].image} alt={concepts[0].title} />
-          <h2>{concepts[0].title}</h2>
-          <p>{concepts[0].description}</p>
-        </li>
-        <li className="concept">
-          <img src={concepts[1].image} alt={concepts[1].title} />
-          <h2>{concepts[1].title}</h2>
-          <p>{concepts[1].description}</p>
-        </li>
-        <li className="concept">
-          <img src={concepts[2].image} alt={concepts[2].title} />
-          <h2>{concepts[2].title}</h2>
-          <p>{concepts[2].description}</p>
-        </li>
+        <Concept
+          image={concepts[0].image}
+          title={concepts[0].title}
+          description={concepts[0].description}
+        />
+        <Concept
+          image={concepts[1].image}
+          title={concepts[1].title}
+          description={concepts[1].description}
+        />
+        <Concept
+          image={concepts[2].image}
+          title={concepts[2].title}
+          description={concepts[2].description}
+        />
       </ul>
     </div>
   );
